50e8eeea4f9d5c6aed3b1b675da30b0ec490351d
Ethan Beach <Beached@mail.uc.edu>
2022-09-25 15:06:17 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Enter Your Age: ");
+        int ageNum = 21;
+        if (ageNum >= 21)
+        {
+            System.out.println("You get a paper wristband!");
+        }
+
+    }
+}
